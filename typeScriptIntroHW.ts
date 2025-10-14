0736fbb25ab6be28d0c9260167d38d5af2690ad0
Mario Iliev <Mario_Iliev@epam.com>
2025-10-14 12:12:13 +0300

    TS intro homework

---
+++ typeScriptIntroHW.ts
@@ -0,0 +1,23 @@
+// Task1
+let name = "John";
+let age = "25";
+
+console.log(`Hello! I am ${name}, ${age} years old.`);
+
+// Task2
+const num1: number = 1;
+
+const num2: number = 1;
+
+console.log('Const addition = ', num1 + num2);
+
+
+// Task3
+let num: number = 51;
+
+
+if (num >= 10 && num <= 50) {
+    console.log(`The number ${num} satisfies the range condition (num >= 10 || num <= 50).`);
+} else {
+    console.log(`The number ${num} does NOT satisfy the range condition (num >= 10 || num <= 50).`);
+}
